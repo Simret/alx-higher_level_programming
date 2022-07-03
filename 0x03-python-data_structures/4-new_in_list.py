1591981cdd46e0b83619fbbb10eb96c7a17f67d9
Simret <simruineb@gmail.com>
2022-07-03 10:44:02 -0700

    Replace element and keep the original array

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    dup = my_list[:]
-    if idx >= 0 and idx < len(dup):
-        dup[idx] = element
-    return dup
+    new_list my_list[:]
+    if idx >= 0 and idx < len(new_list):
+        new_list[idx] = element
+    return new_list
