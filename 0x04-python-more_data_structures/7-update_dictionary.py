9264b401b296079356233130affb8d4a1bcaac55
Simret <simruineb@gmail.com>
2022-07-06 18:14:05 -0700

    Add key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for value in sorted(a_dictionary.keys()):
-        print("{}: {}".format(value, a_dictionary[value]))
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
