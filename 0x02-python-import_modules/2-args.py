7715eac862d735297422f04875f1945387b791f3
Simret <simruineb@gmail.com>
2022-07-06 17:26:38 -0700

    Print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
- from sys import argv
- size = len(argv) - 1
- print("{:d} {:s}{:s}".format(size,
-                              "argument" if size <= 2 and size == 1
-                              else "arguments",
-                              ." if size == 0 else ":"))
- for a, s in enumerate(argv):
-  if a > 0:
-   print("{}: {}".format(a, s))
+    from sys import argv
+    size = len(argv) - 1
+    print("{:d} {:s}{:s}".format(size,
+        "argument" if size <= 2 and size == 
+        else "arguments",
+        ." if size == 0 else ":"))
+    for a, s in enumerate(argv):
+        if a > 0:
+            print("{}: {}".format(a, s))
