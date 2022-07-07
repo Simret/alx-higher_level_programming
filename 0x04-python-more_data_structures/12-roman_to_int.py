0975915322d5de7740f28b15e3b70ac0d568d736
Simret <simruineb@gmail.com>
2022-07-06 18:56:01 -0700

    Roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string and type(roman_string) is str:
-        diction = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+        dictn = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
         value = 0
         for a, x in zip(roman_string, roman_string[1:]):
-            if diction[a] < diction[x]:
-                value -= diction[a]
+            if dictn[a] < dictn[x]:
+                value -= dictn[a]
             else:
-                value += diction[a]
-            value += diction[roman_string[-1]]
+                value += dictn[a]
+            value += dictn[roman_string[-1]]
             return value
-        else:
-            return 0
+    else:
+        return 0
