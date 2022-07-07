11e2a9974243b37c7322ceb0137390e7cb250185
Simret <simruineb@gmail.com>
2022-07-06 17:28:08 -0700

    Print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     size = len(argv) - 1
     print("{:d} {:s}{:s}".format(size,
-        "argument" if size <= 2 and size == 
+        "argument" if size <= 2 and size == 1
         else "arguments",
         ." if size == 0 else ":"))
     for a, s in enumerate(argv):
