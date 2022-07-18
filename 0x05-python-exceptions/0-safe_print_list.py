06a7aa71cdc3ed012713c797dddea207b22f29ac
Simret <simruineb@gmail.com>
2022-07-18 13:25:12 -0700

    Indentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    for y in range(x):
-        try:
-            print("{}".format(my_list[y]), end="")
-            a += 1
-        except IndexError:
-            break
-     print("")
-     return(a)
+ a = 0
+ for y in range(x):
+  try:
+   print("{}".format(my_list[y]), end="")
+   a += 1
+  except IndexError:
+      break
+ print("")
+ return(a)
