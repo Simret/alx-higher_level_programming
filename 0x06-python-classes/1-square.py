28fbe2a671681c17c3703fd3bdd133e66f1a4a05
Simret <simruineb@gmail.com>
2022-07-19 10:39:40 -0700

    Class definition with private field

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
+"""Class Square that defines a square"""
 
 class Square:
-    """ Class Square """
+    '''Class Square'''
     def __init__(self, size):
         self.__size = size
