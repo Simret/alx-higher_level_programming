7c0a3f1e2eaf6309d75745e7199e224e18bed4f0
Simret <simruineb@gmail.com>
2022-07-19 18:10:16 -0700

    Class with method that calculates area and print it

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,12 +6,7 @@ class Square:
     """A class that creates square"""
     def __init__(self, size=0):
         """Initialize fields"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
@@ -34,9 +29,8 @@ class Square:
 
     def my_print(self):
         """Print square"""
-        if self.__size == 0:
-            print('')
-        for x in range(self.__size):
-            for in range(self.__size):
-                print('#', end='')
-            print('')
+        if self.__size != 0:
+            for square in range(self.__size):
+                print('#' * self.size)
+            else:
+                print("")
