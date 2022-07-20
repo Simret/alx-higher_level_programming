46308d024e0d5f10b73a026f7d2db3762217951f
Simret <simruineb@gmail.com>
2022-07-19 18:33:24 -0700

    Class with method that calculates area and print it

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,36 +5,35 @@
 class Square:
     """A class square"""
     def __init__(self, size=0, position=(0, 0)):
-    """Initialize a new square
-    Args:
-        size (int): The size of the new square.
-        position (int, int): The position of the new square."""
+    """Initialize a new square"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get and set the current size of the square"""
+        """Get the current size of the square"""
         return self.__size
     
     @size.setter
     def size(self, value):
+        """Set size of the square""" 
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @propert
+    @property
     def position(self):
-        """Get and set the current position of the square"""
+        """Get the current position of the square"""
         return self.__position
 
     @position.setter
     def position(self, value)
-    if (not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) for num in value) or not all(num >= 0 for num in value)):
-        raise TypeError("position must be a tuple of 2 positive integers")
-    self.__position = value
+        """Set position of the square"""
+        if (not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) for num in value) or not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Calculate the current area of the square"""
