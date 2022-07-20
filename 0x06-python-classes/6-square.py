8d08746512550a9e15797a3fe244f0cbf245ee39
Simret <simruineb@gmail.com>
2022-07-19 18:59:07 -0700

    Class with method that calculates area and print it

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """A class square"""
     def __init__(self, size=0, position=(0, 0)):
-    """Initialize a new square"""
+        """Initialize a new square"""
         self.size = size
         self.position = position
 
@@ -21,7 +21,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -38,7 +39,7 @@ class Square:
         elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-        self.__position = value
+            self.__position = value
 
     def area(self):
         """Calculate the current area of the square"""
