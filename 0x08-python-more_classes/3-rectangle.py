a04b3e4c7781fa46a30cf5ca75002149f49fe91f
Simret <simruineb@gmail.com>
2022-07-25 13:37:50 -0700

    A rectangle class with its attributes and methods defined

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,7 +4,9 @@
 
 class Rectangle:
     """A class that defines a rectangle with attributes and methods"""
+    
     def __init__(self, width=0, height=0):
+
         """A module that initializes a new rectangle
         Args:
             width (int): The width of the new rectangle.
