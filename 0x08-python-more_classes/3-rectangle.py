35fc15e3b778881016be6b1b2b0e733cc245bbf0
Simret <simruineb@gmail.com>
2022-07-25 14:55:37 -0700

    A rectangle class with its attributes and methods defined

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" This module contain a class defining a rectangle"""
+"""A module contain a class defining a rectangle"""
 
 
 class Rectangle:
         """Class that defines a rectangle"""
 
             def __init__(self, width=0, height=0):
-                        """Method that initialize width and height fields"""
-                                self.width = width
-                                self.height = height
+
+                """A Method that initialize width and height attributes"""
+                self.width = width
+                self.height = height
 
             @property
             def width(self):
@@ -23,37 +24,37 @@ class Rectangle:
                     raise ValueError("width must be >= 0")
                 self.__width = value
 
-                @property
-                def height(self):
-                    """Getter method of height"""
-                    return self.height
+            @property
+            def height(self):
+                """Getter method of height"""
+                return self.height
 
-                @height.setter
-                def height(self, value):
-                    """Setter method of height"""
-                    if type(value) != int:
-                        raise TypeError("height must be an integer")
-                    elif value < 0:
-                        raise ValueError("height must be >= 0")
-                    self.__height = value
+            @height.sette
+            def height(self, value)
+                """Setter method of height"""
+                if type(value) != int:
+                    raise TypeError("height must be an integer")
+                elif value < 0:
+                    raise ValueError("height must be >= 0")
+                self.__height = value
 
-                    def area(self):
+                def area(self)
                     """A method that returns the area of the rectangle"""
                     return self.width * self.height
 
-                    def perimeter(self):
-                        """A method that returns the perimeter of the rectangle"""
-                        if self.width == 0 or self.height == 0:
-                            return 0
-                        return ((self.width * 2) + (self.height * 2))
+                def perimeter(self):
+                    """A method that returns the perimeter of the rectangle"""
+                    if self.width == 0 or self.height == 0:
+                        return 0
+                    return ((self.width * 2) + (self.height * 2))
 
-                    def _str__(self):
-                        """A method that returns a printable rectangle"""
-                        rect = ""
-                        if self.height != 0 and self.width != 0:
-                            for i in range(self.height):
-                                for j in range(seelf.width):
-                                    rect += "{}".format("#")
-                                if i < self.height - 1:
-                                    rect += "\n"
-                        return rect
+                def _str__(self):
+                    """A method that returns a printable rectangle"""
+                    rect = ""
+                    if self.height != 0 and self.width != 0:
+                        for i in range(self.height):
+                            for j in range(seelf.width):
+                                rect += "{}".format("#")
+                            if i < self.height - 1:
+                                rect += "\n"
+                    return rect
