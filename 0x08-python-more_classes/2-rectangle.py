492b87e6d83e66e4c9493291455488f513259de3
Simret <simruineb@gmail.com>
2022-07-25 15:00:56 +0000

    Rectangle class with height and weight attributes and area and perimeter methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,151 +1,61 @@
 #!/usr/bin/python3
-
-"""
-
-This module defines a class - Rectangle
-
-"""
-
-
-
+"""A module that defines a rectangle class"""
 
 
 class Rectangle:
-
-    """
-
-    This class has two attributes
-
+    """A class has two attributes
     width
-
     height
-
-    both will have property and setter function definition
-
+    property and setter function definitions
     """
-
-
 
     def __init__(self, width=0, height=0):
-
-        """
-
-        instantiates width and height
-
-        """
-
+        """A method that instantializes width and height"""
         self.width = width
-
         self.height = height
 
-
-
     @property
-
     def width(self):
-
-        """
-
-        function to return width if setter checks have passed
-
-        """
-
+        """Setter method for width"""
         return self.__width
 
-
-
     @width.setter
-
     def width(self, value):
-
-        """
-
-        setter validates if value is >= 0
-
+        """Setter validation if value is >= 0
         Raises:
-
         TypeError
-
         ValueError
-
         """
-
         if not isinstance(value, int):
-
             raise TypeError("width must be an integer")
-
         if value < 0:
-
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
-
-
     @property
-
     def height(self):
-
-        """
-
-        function to return height if setter checks have passed
-
-        """
-
+        """Getter method for height"""
         return self.__height
 
-
-
     @height.setter
-
     def height(self, value):
-
-        """
-
-        setter validates if value is >= 0
-
+        """Setter validation if value is >= 0
         Raises:
-
         TypeError
-
         ValueError
-
         """
-
         if not isinstance(value, int):
-
             raise TypeError("height must be an integer")
-
         if value < 0:
-
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
-
-
     def area(self):
-
-        """
-
-        Returns area of a rectangle using given width and height
-
-        """
-
+        """A method that returns area of a rectangle using given width and height"""
         return (self.width * self.height)
 
-
-
     def perimeter(self):
-
-        """
-
-        Returns perimeter of a rectangle using given width and height
-
-        """
-
+        """A method that returns perimeter of a rectangle"""
         if self.height == 0 or self.width == 0:
-
             return 0
-
         return ((self.width + self.height) * 2)
