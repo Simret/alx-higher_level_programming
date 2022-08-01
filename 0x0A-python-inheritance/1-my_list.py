b09997153c075d3f3a80e18f1a4a31aaf9242485
Simret <simruineb@gmail.com>
2022-08-01 09:58:34 -0700

    A class that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-    1-my_list module
-    Class MyList that inherits from list that contain \
-    Public instance method: def print_sorted(self): that \
-    prints the list, but sorted (ascending sort)
-"""
+"""A module that contains a class which inherits from list"""
+
 
 class MyList(list):
     """A class that inherits from list"""
+
     def print_sorted(self):
         """Print the list sorted ascending order"""
-        print(sorted(self))
+        
+        print(sorted(list(self)))
