b33d76c4aeeb99053c93dfa00a549ed336410c42
Simret <simruineb@gmail.com>
2022-08-01 11:30:33 -0700

    Area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module that checks implementation"""
+
+
+class BaseGeometry:
+    """A class definition"""
+    def area(self):
+        """A method that checks area() implementation"""
+        raise Exception("area() is not implemented")
