ccf47fd93fdaa3552a2fcfa68033c7dc33880cf8
Simret <simruineb@gmail.com>
2022-08-01 10:14:06 -0700

    Check if object is instance of a_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that checks if object is instance of class inherited from a_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """A method that checks if object is instance of a_class"""
+    return isinstance(obj, a_class)
