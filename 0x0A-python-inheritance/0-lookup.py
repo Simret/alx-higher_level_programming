18728eb71cfa90b838f925148c46bb60183b1394
Simret <simruineb@gmail.com>
2022-08-01 10:05:16 -0700

    Available fields and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A function that returns the list of available attributes and methods of an object"""
+"""A function that returns the list of available attributes and methods"""
 
 
 def lookup(obj):
-    """A method that returns the list of available attributes, methods and object"""
+    """A method that returns the list of available attributes and methods"""
     return (dir(obj))
