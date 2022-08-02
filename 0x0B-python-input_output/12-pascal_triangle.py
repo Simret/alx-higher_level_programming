d03ba21d5f413bbbed0762eb19118714fb80490d
Simret <simruineb@gmail.com>
2022-08-02 16:04:39 -0700

    Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module that contains a function returning a list of integers representing the Pascal's triangle of n"""
+
+
+def pascal_triangle(n):
+    """Arguments: n, Return"""
+    lis = []
+
+    if n <= 0:
+        return lis
+
+    for i in range(n):
+        lis.append([])
+        lis[i].append(1)
+        for m in range(1, i):
+            lis[i].append(lis[i - 1][m - 1] + lis[i - 1][m])
+        if n != 0 and i != 0:
+            lis[i].append(1)
+    return lis
