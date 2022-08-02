ff1b7c6803054485ad2bdcbdbfa8c2b3942b6424
Simret <simruineb@gmail.com>
2022-08-01 17:02:31 -0700

    Rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """A module that contains a class with public instance and private getters/setters"""
 
-
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
