74b4f276b895f7a03d138cf1da302774c2f3bf9e
Simret <simruineb@gmail.com>
2022-08-02 14:36:46 -0700

    Object with JSON representation written to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''function definition'''
     with open(filename, mode="w+") as f:
-        json.dump(my_obj, f)i
+        json.dump(my_obj, f)
