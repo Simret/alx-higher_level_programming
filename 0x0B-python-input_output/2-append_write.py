e5de03f61ea5b97a626589debc3ac42169215e81
Simret <simruineb@gmail.com>
2022-08-02 13:53:23 -0700

    Append text and return length of text appended

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''A function that appends characters and returns length of characters appended'''
+
+
+def append_write(filename="", text=""):
+    '''Define a function'''
+    with open(filename, "a", encoding="UTF-8") as file:
+        file.write(text)
+        return len(text)
