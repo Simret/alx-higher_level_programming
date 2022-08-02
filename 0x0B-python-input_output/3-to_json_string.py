051e4eddc7caa70e8772efff5df28e3bf075759f
Simret <simruineb@gmail.com>
2022-08-02 14:15:26 -0700

    Json representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''A function with json representation of a string'''
+
+
+import json
+
+
+def to_json_string(my_obj):
+    '''Function definition'''
+    return json.dumps(my_obj)
