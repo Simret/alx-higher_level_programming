e18dcccea60dbce41f6c9e8b48c912a9adba36e4
Simret <simruineb@gmail.com>
2022-08-02 14:35:18 -0700

    Object with JSON representation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 '''A function that returns an object represented by JSON string'''
-
 import json
 
 
 def from_json_string(my_str):
-    ''Function definition'''
+    '''Function definition'''
     return json.loads(my_str)
