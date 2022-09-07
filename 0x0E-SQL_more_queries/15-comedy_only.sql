aa428b6fc0406de8eb2798f1c0ee99978d3a4f1e
Simret <simruineb@gmail.com>
2022-09-07 16:20:03 -0700

    Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Select comedy shows
+SELECT ts.title FROM tv_shows ts
+INNER JOIN tv_show_genres tg
+ON ts.id = tg.show_id
+INNER JOIN tv_genres tvg
+ON tvg.id = tg.genre_id
+WHERE tvg.name = "Comedy"
+ORDER BY ts.title ASC;
