57e2d073d2e4a62f50538899f038024a93661609
Simret <simruineb@gmail.com>
2022-09-07 16:07:38 -0700

    Join

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Joining tables and querying
+SELECT ts.title, tg.genre_id FROM tv_shows ts
+LEFT JOIN tv_show_genres tg
+ON tg.show_id = ts.id
+WHERE tg.genre_id IS NULL
+ORDER BY ts.title, tg.genre_id ASC;
