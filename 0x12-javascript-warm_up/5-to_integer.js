442b5213e2491aafe3d9baa63b7e5af0533efcd3
Simret <simruineb@gmail.com>
2022-10-03 18:09:29 -0700

    Change permission

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (process.argv[2] && isNaN(process.argv[2]) === false) {
+	console.log('My number: ' + parseInt(process.argv[2], 10));
+} 
+else {
+	console.log('Not a number');
+}
