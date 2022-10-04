9db843227ca3b92ad08d4d171856f13440a4aff0
Simret <simruineb@gmail.com>
2022-10-03 19:32:48 -0700

    Indentation

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ if (!process.argv[2]) {
   console.log('Missing number of occurrences');
 } else {
   for (i = 0; i < process.argv[2]; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 }
