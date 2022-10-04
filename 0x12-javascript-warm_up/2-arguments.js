a7d87bc9cfe29d19d817c1b76fca110c3c6aaeed
Simret <simruineb@gmail.com>
2022-10-03 18:56:49 -0700

    Edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-const { argv } = require('process');
-if (argv.length > 3) {
+if (process.argv[3]) 
+{
 	console.log('Arguments found');
-} else if (argv.length > 2) {
-	console.log('Argument found');
-} else {
+} 
+else if (process.argv[2]) 
+{
+console.log('Argument found');
+}
+else
+{
 	console.log('No argument');
 }
