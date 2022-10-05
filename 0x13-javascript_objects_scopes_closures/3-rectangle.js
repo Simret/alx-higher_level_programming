b6e5f0320e6784cb04ef72d7d2c212c208757631
Simret <simruineb@gmail.com>
2022-10-04 18:35:49 -0700

    Rectangle rotate method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,13 +6,13 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let line = '';
-        for (let j = 0; j < this.width; j++) {
-          line += 'X';
-	}
-  console.log(line);
+print () {
+  for (let i = 0; i < this.height; i++) {
+    let line = '';
+    for (let j = 0; j < this.width; j++) {
+      line += 'X';
+    }
+    console.log(line);
     }
   }
 };
