b5514e7c645da0f9cb2e974cae608f2cb0b97ef2
Simret <simruineb@gmail.com>
2022-10-07 12:18:08 -0700

    Edited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     
     data = sql.connect(user=argv[1], passwd=argv[2], db=argv[3])
     a = data.cursor()
-    a.execute("""SELECT * FROM states ORDER BY id""")
+    a.execute("""SELECT * FROM states ORDER BY states.id ASC""")
     x = a.fetchall()
     for i in x:
         print(i)
