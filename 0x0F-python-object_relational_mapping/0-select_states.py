98b37abc5f016ae94414d75b9e53ecf9e9584033
Simret <simruineb@gmail.com>
2022-10-07 12:07:46 -0700

    Edited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@
 
 if __name__ == "__main__":
     from sys import argv
-    import MySQLdb
+    import MySQLdb as sql
     data = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     a = data.cursor()
     a.execute("""SELECT * FROM states ORDER BY states.id ASC""")
