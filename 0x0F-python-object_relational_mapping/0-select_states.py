3b86fde8704584db5121d21bd97826e41f3564ed
Simret <simruineb@gmail.com>
2022-10-07 12:02:25 -0700

    Edited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """List all states"""
-import MySQLdb 
+import MySQLdb as sql 
 
 
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    data = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    data = sql.connect(user=argv[1], passwd=argv[2], db=argv[3])
     a = data.cursor()
     a.execute("""SELECT * FROM states ORDER BY states.id ASC""")
     x = a.fetchall()
