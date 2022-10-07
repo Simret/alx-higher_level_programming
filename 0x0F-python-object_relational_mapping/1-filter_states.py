4ba3545169ab3622e59350fa34c4f41abd5d69cd
Simret <simruineb@gmail.com>
2022-10-07 11:33:55 -0700

    List state names based on passed argument

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Filter states by name"""
-import MySQLdb as sql
+import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    data = sql.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    data = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     a = data.cursor()
     a.execute("""SELECT * FROM states where name LIKE 'N%'\ ORDER BY states.id ASC")
     x = a.fetchall()
