8ccc98f7e2f85df5e9611047e293e648c3928ff3
Simret <simruineb@gmail.com>
2022-10-07 10:41:24 -0700

    List state names based on passed argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""List state name based on passed argument"""
+import MySQLdb as sql
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    a = data.cursor()
+    a.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(argv[4]))
+    x = a.fetchall()
+    for i in x:
+        if i[1] == state:
+            print(i)
+    a.close()
+    db.close()
