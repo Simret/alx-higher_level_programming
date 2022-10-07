12e4c1175fd53e9d9f5334e8ba3668b0f4a1abcd
Simret <simruineb@gmail.com>
2022-10-07 09:49:02 -0700

    Filter by state name

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Filter states by name"""
 import MySQLdb as sql
-from sys import argv
 
 
-def main(user, password, data):
-    """print states"""
+if __name__ == "__main__":
+    from sys import argv
+    import mySQLdb
     data = sql.connect(host='localhost', user=user, password=password, db=data, port=3306)
     a = data.cursor()
     a.execute("""SELECT * FROM states where name LIKE 'N%'\ ORDER BY states.id ASC")
@@ -13,7 +13,5 @@ def main(user, password, data):
     for i in x:
         if i[1][0] == 'N':
             print(i)
-
-
-if __name__ == '__main__':
-main(argv[1], argv[2], argv[3])
+    a.close()
+    data.close()
