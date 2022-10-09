2c4fa9d14d09a1f3c4709d07ceea088b56d82809
Simret <simruineb@gmail.com>
2022-10-09 13:47:50 -0700

    7 edited

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """List all state objects"""
-if__name__ == "__main__":
-        from sys import argv
-        from model_state import Base, State
-        from sqlalchemy import (create_engine)
-        from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
-        engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engin)
-        Session = sessionmaker(bind=engin)
-        s = Session()
-        for st in s.query(State).order_by(State.id):
-            print("{:d}: {:s}".format(st.id, st.name))
-        session().close()
+def main(user, password, data):
+    engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, data), pool_pre_ping=True)
+    Base.metadata.create_all(engin)
+    Session = sessionmaker(bind=engin)
+    s = Session()
+    for st in s.query(State).order_by(State.id):
+        print("{:d}: {:s}".format(st.id, st.name))
+        
+
+if __name__ == '__main__':
+    main(argv[1], argv[2], argv[3])
