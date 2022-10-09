d15f5435ce21eb9039e60c49d7503162afdf9823
Simret <simruineb@gmail.com>
2022-10-09 13:56:48 -0700

    8 edited

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """Script that prints the first state obj, takes 3 args"""
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    from sys import argv
-    from model_state import State, Base
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    session = sessionmaker(bind=engine) 
-    Base.metadata.create_all(engine)
-    st = session().query(state).first()
+def main(user, password, data):
+    engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, data), pool_pre_ping=True)
+    Base.metadata.create_all(engin)
+    Session = sessionmaker(bind=engin)
+    s = Session()
+    st = s.query(state).first()
     if st:
-        print("{}: {}".format(st.id, st.name))
+        print("{:d}: {:s}".format(st.id, st.name))
     else:
         print("Nothing")
-    session().close()
+        
+
+if __name__ == '__main__':
+    main(argv[1], argv[2], argv[3])
