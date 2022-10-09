82ae23033134002d5cb3324be200beabb83db802
Simret <simruineb@gmail.com>
2022-10-09 11:58:03 -0700

    Delete

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     import MySQLdb
     data = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     a = data.cursor()
+    check = (argv[4], )
     a.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id ASC", check)
     x = a.fetchall()
     for i in x:
