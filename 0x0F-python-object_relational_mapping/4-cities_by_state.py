ffb39215ee759d67f2806352552ca88cc8fe2e83
Simret <simruineb@gmail.com>
2022-10-09 12:16:10 -0700

    4 edited

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,13 +2,12 @@
 """List states in database"""
 
 
-def main(user, password, data):
+if __name__ == "__main__":
     from sys import argv
     import MySQLdb
     data = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     a = data.cursor()
-    a.execute("SELECT cities.id, cities.name, states.name FROM cities\
-    JOIN states ON states.id = cities.state_id ORDER BY id ASC;")
+    a.execute("SELECT * FROM cities ORDER BY id ASC;")
     x = a.fetchall()
     for i in x:
         print(i)
