62ea5ba8e59042674c6d63d05d0641bef7539c25
simret <simruineb@gmail.com>
2022-10-21 04:02:35 +0300

    pip

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,17 +3,15 @@
 
 
 def find_peak(list_of_integers):
-    """
-    function to find peak
-    """
-    l = len(list_of_integers)
-    if l == 0:
+    """Find peak"""
+    ln = len(list_of_integers)
+    if ln == 0:
         return
-    half = l // 2
-    if (half == l - 1 or list_of_integers[half] >=
+    half = ln // 2
+    if (half == ln - 1 or list_of_integers[half] >=
         list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
                                          list_of_integers[half - 1]):
         return (list_of_integers[half])
-    elif half != l - 1 and list_of_integers[half + 1] > list_of_integers[half]:
+    elif half != ln - 1 and list_of_integers[half + 1] > list_of_integers[half]:
         return (find_peak(list_of_integers[half + 1:]))
     return (find_peak(list_of_integers[:half]))
