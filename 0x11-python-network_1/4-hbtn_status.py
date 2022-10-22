e13678eddb16b820bb748ba53fa435769896f443
simret <simruineb@gmail.com>
2022-10-22 03:21:19 +0300

    Using requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    reply = requests.get('https://alxxintranet.hbtn.io/status').text
+    reply = requests.get('https://alx-intranet.hbtn.io/status').text
     print("Body response:")
     print("\t- type: {}".format(type(reply)))
     print("\t- content: {}".format(reply))
