08ab125bb998d0f9272eab5777851fe2bd7e60d7
simret <simruineb@gmail.com>
2022-10-22 03:58:29 +0300

    Resp header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Send a request to the url and display the values of the variable X-Request-Id"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print("{}".format(r.headers.get('X-Request-Id')))
