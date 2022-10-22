d60a45b8b6803b05d2a52875b79139d1ab6bc2f3
simret <simruineb@gmail.com>
2022-10-22 09:00:52 +0300

    Catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends request to url
-curl -sL 0:5000/catch_me -X PUT "You got me!" -d "user_id=98" 
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,15 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    """Find peak"""
-    ln = len(list_of_integers)
-    if ln == 0:
-        return
-    half = ln // 2
-    if (half == ln - 1 or list_of_integers[half] >=
-        list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
-                                         list_of_integers[half - 1]):
-        return (list_of_integers[half])
-    elif half != ln - 1 and list_of_integers[half + 1] > list_of_integers[half]:
-        return (find_peak(list_of_integers[half + 1:]))
-    return (find_peak(list_of_integers[:half]))
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
