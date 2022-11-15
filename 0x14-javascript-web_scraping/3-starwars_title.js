3b3499a892540e1920c9c35613b77d161ac78e3c
Simret <simruineb@gmail.com>
2022-11-15 23:27:12 +0000

    End point corrected

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request.get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, response, body) {
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, response, body) {
   if (err) throw err;
   else if (response.statusCode === 200) {
     console.log(JSON.parse(body).title);
