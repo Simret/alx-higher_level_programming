80448bd6ba3178a8c78d23ab40b082c223f91e3c
simret <simruineb@gmail.com>
2022-11-16 11:02:58 +0300

    Semistandard fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const request = require('request')
-const api = 'https://swapi-api.hbtn.io/api/films/'
+const request = require('request');
+const api = 'https://swapi-api.hbtn.io/api/films/';
 request.get(api + process.argv[2], function (err, response, body) {
-  if (err) throw err
+  if (err) throw err;
   else if (response.statusCode === 200) {
-    const everything = JSON.parse(body)
+    const everything = JSON.parse(body);
     for (const ch of everything.characters) {
       request.get(ch, function (err, response, body) {
-        const everything = JSON.parse(body)
-        if (err) throw err
-        else if (response.statusCode === 200) { console.log(everything.name) }
-      })
+        const everything = JSON.parse(body);
+        if (err) throw err;
+        else if (response.statusCode === 200) { console.log(everything.name); }
+      });
     }
   }
-})
+});
